4ca9e9728919724073a7284269ed3d682e6706e1
Sujal Badal <badalsujalbadal@gmail.com>
2026-04-25 12:15:31 +0530

    Learn basics

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,32 @@
+// import React from 'react'
+
+// const App = () => {
+//   return (
+//     <div id="parent">
+//       <h1 id="child1">Hello</h1>
+//       <h2 id="child2">Use of div</h2>
+//     </div>
+//   )
+// }
+
+// export default App
+
+//use of fragment
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div id="child1">
+      <h1 >Hello</h1>
+    </div>
+    <div id="child1">
+      <h1 >Use of fragement</h1>
+    </div>
+    </>
+   
+  )
+}
+
+export default App
+
